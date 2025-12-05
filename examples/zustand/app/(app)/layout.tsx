7c94324258662a2695bcfe8a8a06427f83e7595b
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2025-12-05 11:23:30 +0900

    fix: zustand build error 수정

--- examples/zustand/app/(app)/layout.tsx
+++ examples/zustand/app/(app)/layout.tsx
@@ -1,11 +1,13 @@
 import { CartStoreProvider } from "@/providers/cart-store-provider";
 import { getCarts, getQueryClient } from "@repo/shared";
+import { connection } from "next/server";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  await connection();
   const queryClient = getQueryClient();
   await queryClient.prefetchQuery({
     queryKey: ["carts"],
