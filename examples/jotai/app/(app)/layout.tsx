43e87483c3d249cccf9f887b82ae4157451f194d
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2025-12-05 11:06:19 +0900

    fix: jotai build error 수정

--- examples/jotai/app/(app)/layout.tsx
+++ examples/jotai/app/(app)/layout.tsx
@@ -1,11 +1,13 @@
 import HydrateCartAtom from "@/components/HydrateCartAtom";
 import { getCarts, getQueryClient, Hydrate } from "@repo/shared";
+import { connection } from "next/server";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  await connection();
   const queryClient = getQueryClient();
   await queryClient.prefetchQuery({
     queryKey: ["carts"],
